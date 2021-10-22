113fd18bda654df1fb53ea8de415a69c3d69af60
Daniil Ketov <danielketovone@gmail.com>
2021-10-22 22:58:36 +0500

    Upload build

---
+++ react-weather-app/build/precache-manifest.46e9d59ca618bbf683cb2fe8fbc20a65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0840b1532cd9f37645e94d66c1ee13b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8000dacd22f5f715fc3f",
+    "url": "/static/js/2.f1b885d8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f1b885d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "370854775d508a63c5c3",
+    "url": "/static/js/main.f7c27a55.chunk.js"
+  },
+  {
+    "revision": "3b906a68717433f63125",
+    "url": "/static/js/runtime-main.33336675.js"
+  },
+  {
+    "revision": "668bcf5dc9baf8f938e49b8d65464bd8",
+    "url": "/static/media/default.668bcf5d.png"
+  }
+]);
